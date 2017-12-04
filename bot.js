a839e79997f5c28199f89e34c0d3c53075977e9a
Dmanwaring <dmanny70@gmail.com>
2017-12-04 17:04:10 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,12 +27,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch(cmd) {
             // .ping
-            case 'ping':
+            case "ping":
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: "Pong!"
                 });
             break;
+                 case "info" :
+    message.channel.send("I am CUBE, a Celestial AI created by Sam'anar. My purpose is to provide knowledge about the Galaxy to you, its mortal inhabitants.");
+    break;
+}
             // Just add any case commands if you want to..
          }
      }
