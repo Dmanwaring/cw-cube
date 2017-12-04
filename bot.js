f9c9ef7c8fdf34a6f44178635b70c1d424e41044
Dmanwaring <dmanny70@gmail.com>
2017-12-04 18:57:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
-    bot.user.setGame('Under Construction');
+    client.user.setGame('Under Construction');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
